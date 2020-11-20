29315b43c18ccbe64440e68cbed27bcda5fdb29c
Isaac Basque-Rice <ibasquerice@gmail.com>
2020-11-20 00:16:17 +0000

    more validation

--- LogIn.php
+++ LogIn.php
@@ -24,19 +24,61 @@
 		<?php include 'master/header.php'?>
         
         <br/>
+		
+		<?php
+			// Global Variables
+			$email = $pword = "";
+			
+			if ($_SERVER["REQUEST_METHOD"] == "POST") {
+				$email = validateEmail($_POST["email"]);
+				$pword = validatePassword($_POST["pword"]);
+			}
+			function validateEmail($info) {
+				if ($info == "") {
+					echo "<p class='error'>Please input your email address</p>";
+				} else {
+					$info = strip($info);
+					if (!(filter_var($info, FILTER_VALIDATE_EMAIL))) {
+						echo "<p class='error'>This email address is invalid</p>";
+					}
+				}
+				return $info;
+			}
+			function validatePassword($info) {
+				if ($info == "") {
+					echo "<p class='error'>Please choose a password</p>";
+				} else {
+					$info = strip($info);
+				}
+				return $info;
+			}
+			
+			function strip($info)
+			{
+				// done to avoid XSS and SQLi
+				$info = trim($info); // removes extra space, tab, newline, etc
+				$info = stripslashes($info); // removes slashes
+				$info = htmlspecialchars($info); // removes &,",',<,>
+				
+				return $info;
+			}
+		?>
         
         <div class="container">
             <h1>Log In</h1>
             <form action="master/check.php" method="post">
                 <div class="form-group">
                     <label for="email" class="formLabel">Email address</label>
-                    <input type="email" class="form-control" id="email" placeholder="Enter email">
+                    <input type="email" class="form-control" id="email" name="email" value="<?php echo $email ?>"
+						   placeholder="Enter email">
                 </div>
                 <div class="form-group">
                     <label for="password" class="formLabel">Password</label>
-                    <input type="password" class="form-control" id="password" placeholder="Password">
+                    <input type="password" class="form-control" id="password" name="pword" value="<?php echo $pword ?>"
+						   placeholder="Password">
                 </div>
-                <button type="submit">Submit</button>
+                <button type="submit" id="submit">Submit</button>
+				<button type="reset" id="reset">Reset</button>
             </form>
             <br/>
             <p>Don't have an account?</p>
