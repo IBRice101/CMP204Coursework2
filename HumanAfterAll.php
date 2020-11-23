bd1d80182cbf0e10e03454cee614cbe04e6f487a
Isaac Basque-Rice <ibasquerice@gmail.com>
2020-11-23 01:00:46 +0000

    removed unnecessary script tags

--- HumanAfterAll.php
+++ HumanAfterAll.php
@@ -18,7 +18,6 @@
         <!--Scripts-->
         <script src="https://code.jquery.com/jquery-3.5.1.js"
                 integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="../scripts/iframeReplace.js"></script>
         <script type="text/javascript" src="scripts/themeChange.js"></script>
     </head>
     <body>
