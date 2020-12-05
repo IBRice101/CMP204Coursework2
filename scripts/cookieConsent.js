ec68ee8a253ab607ce14d62bbd681e02d0394522
Isaac Basque-Rice <ibasquerice@gmail.com>
2020-12-05 15:17:42 +0000

    edited cookie consent popup content

--- scripts/cookieConsent.js
+++ scripts/cookieConsent.js
@@ -1,2 +1,2 @@
-window.start.init({Palette:"palette1",Theme:"classic",Message:"This website uses cookies, they hold no data on" +
-        " you, however we will inform you if this changes.",})
+window.start.init({Palette:"palette1",Theme:"classic",Message:"This website uses cookies in order to keep you " +
+        "logged in until you manually sign out.",})
