ec870cf0804215228b23f256fea42f9cd3f247ac
Isaac Basque-Rice <ibasquerice@gmail.com>
2020-12-10 19:46:27 +0000

    converted to PHP

---
+++ blogposts/post1.php
@@ -0,0 +1,67 @@
+<h1> Top 4 Daft Punk Albums </h1>
+
+![Daft Punk at the premiere of Tron: Legacy in 2010.](https://upload.wikimedia.org/wikipedia/commons/4/41/Daftpunklapremiere2010.jpg)
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel dolor non magna elementum sagittis eget auctor
+	elit. Sed id arcu nunc. Etiam pretium velit a libero facilisis, in ullamcorper lorem feugiat. Curabitur pharetra
+	vestibulum pellentesque. Mauris a dapibus tellus. Vestibulum eu pretium nulla.</p>
+
+<h2> 1. Random Access Memories </h2>
+
+<img src="https://upload.wikimedia.org/wikipedia/en/a/a7/Random_Access_Memories.jpg" alt="The cover of Random Access Memories">
+
+<p>Integer euismod lorem et neque venenatis, et pharetra quam ultrices. Praesent ipsum purus, blandit vel molestie eu,
+	congue ut nibh. Donec eu dapibus nisl. Maecenas molestie elit vel odio auctor, venenatis dignissim urna vulputate.
+	Nullam varius purus faucibus, molestie lorem eu, facilisis sem. Quisque lobortis lorem et orci ultricies finibus.
+	Vivamus a libero aliquam, tempor purus sed, facilisis nunc. Aenean vitae dolor posuere, hendrerit risus eget,
+	condimentum mi. Ut pulvinar lacus a diam sodales dignissim. Aliquam sit amet quam erat. Donec ac nisl ullamcorper,
+	ornare libero convallis, egestas felis. Donec tempor feugiat elementum. Sed molestie at nibh molestie gravida.
+	<br/>
+	Sed nisi neque, porttitor a posuere ut, porttitor a metus. Phasellus ipsum turpis, vulputate nec quam nec, sagittis
+	vulputate nibh. Vivamus porttitor iaculis tincidunt. Vestibulum orci lorem, viverra nec commodo eu, rutrum ut metus.
+	Quisque lobortis elit sit amet augue iaculis sodales. Sed id lectus lacinia, gravida neque lobortis, luctus quam.
+	Phasellus nec nunc eu diam venenatis pellentesque.</p>
+
+<h2> 2. Discovery </h2>
+
+<img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Daft_Punk_-_Discovery.jpg/220px-Daft_Punk_-_Discovery.jpg" alt='The standard release cover: a black background with liquid metal forming the words "Daft Punk"'>
+
+<p>Nullam mollis enim nisi, et lobortis neque lacinia eu. Nulla elementum, sem id ornare molestie, arcu turpis elementum
+	lorem, vel efficitur orci diam vitae lectus. Nullam lectus nibh, consequat maximus sollicitudin vitae, sollicitudin
+	sed velit. Donec justo risus, fringilla ut tellus sed, placerat condimentum massa.
+	<br/>
+	Vivamus mollis nisl ac quam porttitor pellentesque. Cras porta turpis sed justo vestibulum vulputate. Sed nec
+	ullamcorper risus. Duis porta suscipit ante, in condimentum nunc accumsan sed. In vitae leo sit amet lacus imperdiet
+	dapibus. Suspendisse vestibulum volutpat felis, vel pretium leo convallis quis. Pellentesque efficitur tellus vel
+	neque feugiat congue. Proin sit amet dolor ac lectus elementum semper vel sed leo. Nunc eget lorem malesuada,
+	vulputate nibh ut, varius nisl. Nullam eu iaculis velit. Integer ac tortor ac magna ornare volutpat sit amet vitae
+	quam. Fusce in laoreet justo.</p>
+
+<h2> 3. Homework </h2>
+
+<img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9c/Daftpunk-homework.jpg/220px-Daftpunk-homework.jpg" alt="The cover of Homework">
+
+<p> Pellentesque elementum malesuada auctor. Mauris nec nibh hendrerit, viverra lacus nec, finibus felis. Donec sed
+	tortor  consectetur, faucibus ex vitae, maximus nunc. Donec vel sapien quis libero fermentum lobortis sed a dui.
+	Quisque laoreet , justo ut auctor dapibus, mauris sem tristique est, in feugiat felis justo eu leo. Curabitur
+	consectetur tempor lectus, eu pellentesque justo egestas in. Ut sit amet mattis elit. Pellentesque quis dolor lorem.
+	Aenean ac interdum justo. Etiam venenatis ipsum eu ante mollis, eu finibus metus consequat. Quisque egestas odio at
+	eleifend hendrerit. Maecenas ac augue vel felis lacinia dapibus sed ut massa. Integer ut laoreet turpis, eu auctor
+	urna. Quisque fringilla metus a 	tincidunt viverra. Cras augue velit, lacinia sed mollis non, facilisis quis
+	neque. </p>
+
+<h2> 4. Human After All </h2>
+
+![Humanafterall.jpg](https://upload.wikimedia.org/wikipedia/en/thumb/0/0d/Humanafterall.jpg/220px-Humanafterall.jpg)
+
+<img src="https://upload.wikimedia.org/wikipedia/en/thumb/0/0d/Humanafterall.jpg/220px-Humanafterall.jpg" alt="The cover of Human After All">
+
+<p>Proin sed ex tincidunt, sollicitudin risus et, venenatis turpis. Etiam et libero luctus, pulvinar purus at, venenatis
+	magna. Maecenas ut rhoncus nisi. Donec convallis tellus sed imperdiet lacinia. Aliquam iaculis eget lectus nec
+	sodales. Fusce viverra consequat turpis id dictum. Donec interdum, risus non consectetur ultricies, enim dolor
+	interdum ante, sit amet sodales est ligula sed quam. Ut varius malesuada feugiat. Quisque sapien justo, dignissim
+	sit amet lorem in, cursus dictum mauris. Nam consectetur odio ligula, in pulvinar ipsum venenatis in. Morbi lectus
+	felis, mollis vitae convallis vel, imperdiet non risus. Orci varius natoque penatibus et magnis dis parturient
+	montes, nascetur ridiculus mus. Praesent risus tellus, porta nec efficitur lobortis, rutrum eget nunc. Maecenas
+	tristique, quam a molestie bibendum 	, velit enim finibus turpis, nec tincidunt tortor dolor sed libero. Nulla
+	auctor gravida nibh id aliquam. </p>
---
+++ blogposts/post2.php
@@ -0,0 +1,29 @@
+<h1> Where are Daft Punk? </h1>
+
+<img src="https://weraveyou.com/wp-content/uploads/2020/10/Daft-Punk.jpg" alt="Daft Punk Against a blue background">
+
+<p>Nulla facilisi. Duis sit amet maximus massa. Morbi sem nisl, euismod ac viverra in, aliquet at lectus. Integer
+	pulvinar quis erat pulvinar aliquam. Mauris eu dignissim purus. Cras vitae vulputate orci. Sed suscipit gravida
+	nibh eu maximus. In posuere quis risus nec ultrices.</p>
+
+<img src="https://www.biography.com/.image/t_share/MTQ3NjM5NjAyMDAwMTc2MjAy/pharrell_williams_photo_by_brian_bowen_nbcuniversal_getty_462041114.jpg" alt="Pharrell Williams - Music Producer, Singer">
+
+<p>Vestibulum aliquet, ipsum nec lobortis venenatis, libero sem tristique felis, vitae maximus lectus ex eu lorem.
+	Integer efficitur risus odio, et accumsan eros pretium ac. Duis sodales velit at convallis aliquet. Vivamus congue,
+	ligula faucibus rhoncus tristique, ex justo varius nisi, in pulvinar felis justo ut magna. Praesent viverra aliquet
+	felis, quis euismod sapien. Sed vulputate, felis eu eleifend fermentum, turpis ante semper ante, vel pretium est
+	nibh a sapien. Donec congue lacinia imperdiet. In nisi lorem, aliquet luctus feugiat at, interdum in leo. Ut posuere
+	nisi in odio finibus, non semper elit ullamcorper. Donec ultricies, quam eu suscipit maximus, turpis lorem sagittis
+	nibh, at dictum nisi augue sed enim. </p>
+
+<p>Ut suscipit enim ac lorem aliquet, eget vehicula nisl ullamcorper. Integer a laoreet sem. Vivamus nisi erat, pharetra
+	sed nunc ut, porttitor aliquam massa. Nulla a dolor sit amet nisl posuere feugiat. Nam ultrices nulla vel molestie
+	sodales. Donec vel dui mi. Sed egestas sapien scelerisque, vestibulum turpis quis, tempus erat. Nam hendrerit
+	hendrerit enim eget elementum. </p>
+
+<img src="https://www.nme.com/wp-content/uploads/2018/10/PARCELS_MAY2018_3_image_AntoineHenault.jpg" alt="Parcels promo image for their debut album">
+
+<p>Ut efficitur sit amet elit non vulputate. Integer nisl felis, fringilla eu mollis ut, finibus vitae ante. Nam ac
+	massa in dolor vestibulum posuere non vitae augue. Nam mollis condimentum ante eget rhoncus. Nulla egestas ornare
+	nibh eu accumsan. Nulla eleifend varius turpis at pellentesque. Mauris quam ligula, mattis quis auctor ut, fermentum
+	blandit arcu. </p>
